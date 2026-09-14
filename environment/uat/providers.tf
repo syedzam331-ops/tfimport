2ae1d357ebc95d3049add9e3540c8b18e310620b
SurendraBabuRN <surendrababu1037@gmail.com>
2026-09-14 15:10:42 +0530

    Update providers.tf

--- environment/uat/providers.tf
+++ environment/uat/providers.tf
@@ -11,11 +11,12 @@ terraform {
     storage_account_name = "storageimportsuri"
     container_name       = "import-test"
     key                  = "terraform.tfstate"
+    use_azuread_auth     = true
   }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "71a493c0-e420-4676-ab7c-c5d0ea51284a"
-  use_oidc = true
+  use_oidc        = true
 }
