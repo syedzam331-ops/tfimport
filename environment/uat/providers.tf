7bc259ce3d2faf3a2db2406f476390668727e3d6
SurendraBabuRN <surendrababu1037@gmail.com>
2026-09-14 14:30:43 +0530

    Create providers.tf

---
+++ environment/uat/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "suri-import-rg"
+    storage_account_name = "storageimportsuri"
+    container_name       = "import-test"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "71a493c0-e420-4676-ab7c-c5d0ea51284a"
+}
