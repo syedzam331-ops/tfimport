1b14fc0546699a2eda3b3d69f6bd883a85d9ea22
SurendraBabuRN <181931928+SurendraBabuRN@users.noreply.github.com>
2026-09-14 11:44:41 +0000

    Auto-generated Terraform code from existing Azure resources

---
+++ environment/uat/generated_resources.tf
@@ -0,0 +1,10 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "/subscriptions/71a493c0-e420-4676-ab7c-c5d0ea5128a4/resourceGroups/uat-rg01"
+resource "azurerm_resource_group" "uat_rg" {
+  location   = "eastus"
+  managed_by = null
+  name       = "uat-rg01"
+  tags       = {}
+}
